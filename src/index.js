de4d3c6a4898f04df8b804f874b7238046df7c8d
EdFazli <edfazli92@gmail.com>
2021-01-17 04:50:08 +0800

    workingbasicauthentication

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from "./Router";
+import "antd/dist/antd.css";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
 import reportWebVitals from './reportWebVitals';
 
+Amplify.configure(config)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router />,
   document.getElementById('root')
 );
 
