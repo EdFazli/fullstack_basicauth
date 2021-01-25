8a3a1757f39918b392cfa84090ce885c8396a8a8
EdFazli <edfazli92@gmail.com>
2021-01-25 22:37:03 +0800

    protectedjs

--- src/Protected.js
+++ src/Protected.js
@@ -1,14 +1,9 @@
 import React, {useEffect} from "react";
 import { Auth } from "aws-amplify";
 import Container from "./Container";
+import { protectedRoute } from "./protectedRoute";
 
-function Protected(props) {
-    useEffect(() => {
-        Auth.currentAuthenticatedUser()
-            .catch(() => {
-                props.history.push("/profile")
-            })
-    }, [])
+function Protected() {
     return (
         <Container>
             <h1>Protected Route!!!</h1>
@@ -16,4 +11,4 @@ function Protected(props) {
     );
 }
 
-export default Protected
+export default protectedRoute(Protected)
