a7f1521b855a41ec4b63f4268ab443bef6192f02
EdFazli <edfazli92@gmail.com>
2021-01-26 23:29:27 +0800

    componentupdatedinformjs

--- src/Form.js
+++ src/Form.js
@@ -6,6 +6,55 @@ import ConfirmSignUp from './ConfirmSignUp';
 import ForgotPassword from "./ForgotPassword";
 import ForgotPasswordSubmit from './ForgotPasswordSubmit';
 
+async function signIn({ username, password }, setUser) {
+    try {
+        const user = await Auth.signIn(username, password)
+        const userInfo = { username: user.username, ...user.attributes }
+        setUser(userInfo)
+    } catch(err) {
+        console.log("error signing up..", err)
+    }
+}
+
+async function signUp({ username, password, email }, updateFormType) {
+    try {
+        await Auth.signUp({
+            username, password, attributes: { email }
+        })
+        console.log("sign in success!")
+        updateFormType("confirmSignUp")
+    } catch (err) {
+        console.log("error signing up..", err)
+    }
+}
+
+async function confirmSignUp({ username, confirmationCode }, updateFormType) {
+    try {
+        await Auth.confirmSignUp(username, confirmationCode)
+        updateFormType("signIn")
+    } catch (err) {
+        console.log("error signing up..", err)
+    }
+}
+
+async function ForgotPassword({ username}, updateFormType) {
+    try {
+        await Auth.forgotPassword(username)
+        updateFormType("forgotPasswordSubmit")
+    } catch (err) {
+        console.log("error submitting username to reset password...", err)
+    }
+}
+
+async function forgotPasswordSubmit({ username, confirmationCode, password }, updateFormType) {
+    try {
+        await Auth.forgotPasswordSubmit(username, confirmationCode, password)
+        updateFormType("signIn")
+    } catch (err) {
+        console.log("error updating password...", err)
+    }
+}
+
 const initialFormState = {
     username: '', password: '', email: '', confirmationCode: ''
 }
